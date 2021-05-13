86e90d62743dd1faab5946a0acc4717b7568e49d
SoniaSayago <75644975+SoniaSayago@users.noreply.github.com>
2021-05-12 19:52:02 -0500

    jugando con mi servidor web

--- index.js
+++ index.js
@@ -3,25 +3,31 @@ const json = require('./package.json');
 
 const server = http.createServer(function (req, res) {
     const versionJSON = {"version": json.version};
-    const data = {"data": 'Hola mundo'};
+    const descriptionJSON = {"description": json.description};
     if (req.url === '/'){
         res.writeHead(200, {'Content-Type': 'application/json'});
         res.write(JSON.stringify(versionJSON));
         res.end(' versión creada');
     }
 
-    if (req.url === '/hola'){
+    else if (req.url === '/description'){
         res.writeHead(200, {'Content-Type': 'application/json'});
-        res.write(JSON.stringify(data));
-        res.end(' tu primer hola mundo');
+        res.write(JSON.stringify(descriptionJSON));
+        res.end();
     }
+
+    else if (req.url === '/description'){
+        const ip = res.socket.remoteAddress;
+        const port = res.socket.remotePort;
+        res.end(`Your IP address is ${ip} and your source port is ${port}.`);
+    }
+
     else if (req.url === '/hora'){
         const today = new Date();
         res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
         res.write(`La fecha y hora de hoy es ${today.toISOString()}.`);
         res.end();
     } 
-
     else { 
         res.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'});
         res.write('Ruta inválida, intenta otra ruta');
