839f1d84c53da5a3b1239296a45c2b35ef67588c
SoniaSayago <75644975+SoniaSayago@users.noreply.github.com>
2021-05-12 19:37:00 -0500

    1er data server

--- index.js
+++ index.js
@@ -3,23 +3,29 @@ const json = require('./package.json');
 
 const server = http.createServer(function (req, res) {
     const versionJSON = {"version": json.version};
+    const data = {"data": 'Hola mundo'};
     if (req.url === '/'){
         res.writeHead(200, {'Content-Type': 'application/json'});
         res.write(JSON.stringify(versionJSON));
-        res.end('versión creada');
+        res.end(' versión creada');
     }
 
+    if (req.url === '/hola'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify(data));
+        res.end(' tu primer hola mundo');
+    }
     else if (req.url === '/hora'){
         const today = new Date();
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.write(today.toISOString());
-        res.end(`La fecha y hora de hoy es ${today}.`);
+        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+        res.write(`La fecha y hora de hoy es ${today.toISOString()}.`);
+        res.end();
     } 
 
     else { 
         res.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'});
         res.write('Ruta inválida, intenta otra ruta');
-        res.end('Ruta inválida');
+        res.end();
     }
     
 }).listen(5000);
